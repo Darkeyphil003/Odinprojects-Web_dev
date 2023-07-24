ec82aee0cf4c323c276242387691f7bdd6e17c01
Josh <officialdarkey02@yahoo.com>
2023-07-24 17:04:24 +0800

    first submission of projects

---
+++ Javascript-projects/first.js
@@ -0,0 +1,13 @@
+
+let add7 = (number) =>  number + 7;
+
+let multiply = (first,second) => first * second
+
+let capitalize = (string) => string.toUpperCase()[0];
+
+let lastLetter = (string) => {  return string[string.length-1];}
+
+console.log(add7(2));
+console.log(multiply(1,2));
+console.log(capitalize("bruh"));
+console.log(lastLetter("bruh"));
