b0f8d6bad7fd8a79cadb18ac319287340a48cf73
Josh <officialdarkey02@yahoo.com>
2023-07-30 21:44:54 +0800

    working on the rockpaperscissor game
    rockpapaer scissor game progression

--- Javascript-projects/html-proj/first/rock-paper-scissors/rockPaperScissor.js
+++ Javascript-projects/html-proj/first/rock-paper-scissors/rockPaperScissor.js
@@ -1,1 +1,39 @@
+    const meme_pics  = [
+        'images/handsome-squidward.jpg',
+        'images/mike.jpg',
+        'images/pepe.jpg'
+    ]
+    const playThing = [
+            'images/rock.png',
+            'images/scissor.png',
+            'images/paper.png'
+    ]
+    let computer_input_score;
+    let user_input_score;
+    let updateRound = () => {
+        const roundCount = document.querySelector(".round-count");
+    
+    }
+
+   
+    let setComputerInputScore = () =>{
+
+    }
+
+    const getRandomImage = () =>{
+        const randomIndex = Math.floor(Math.random() * meme_pics.length);
+        return meme_pics[randomIndex];
+      } 
+
+    const setRandomImage = () => {
+        const imageRandomIndexCpu = getRandomImage();
+        const imageRandomIndexUser = getRandomImage();
+        const randomImageCpu = document.querySelector(".cpu-img");
+        const randomImageUser = document.querySelector(".user-img");
+        
+
+        randomImageCpu.src = imageRandomIndexCpu;
+        randomImageUser.src = imageRandomIndexUser;
+}
+    setRandomImage();
     
