cc7b6d5613507c222f0167b8006bebca5f3dcb29
Josh <officialdarkey02@yahoo.com>
2023-08-01 15:26:29 +0800

    Done with the round counter logic

--- Javascript-projects/html-proj/first/rock-paper-scissors/rockPaperScissor.js
+++ Javascript-projects/html-proj/first/rock-paper-scissors/rockPaperScissor.js
@@ -10,13 +10,29 @@
     ]
     let computer_input_score;
     let user_input_score;
-    let updateRound = () => {
-        const roundCount = document.querySelector(".round-count");
+
+    //  roundCount Logic:
+    //  I have to get the element object from the .round-count class
+    //  after that, i created another variable called current round which starts at 0
+    //  after getting access with the .round-count class i assign the variable currentRound into it.
     
+    let roundCount = document.querySelector(".round-count");
+    let currentRound = 0;
+    roundCount.textContent = currentRound;
+
+    //  updateRound logic:
+    //  after assigning the currentRound into the roundCount class
+    //  i add + 1 into it in order to update it
+    // and simply assign the new value back into the textcontent of roundCount
+    updateRound = () => {
+        currentRound+=1;
+        roundCount.textContent = currentRound;
     }
 
-   
 
+
+   
+   
 
     const getRandomImage = () =>{
         const randomIndex = Math.floor(Math.random() * meme_pics.length);
@@ -33,4 +49,7 @@
         randomImageUser.src = imageRandomIndexUser;
 }
     setRandomImage();
+    
+   
+    
     
