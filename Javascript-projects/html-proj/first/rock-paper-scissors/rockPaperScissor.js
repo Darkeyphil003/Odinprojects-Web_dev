a9b46a749e6832985cce26df4e6dcff66bb88390
Josh <officialdarkey02@yahoo.com>
2023-08-03 17:01:28 +0800

    done with the evaluator logic
    Just need to make it so it works with the whole site... lots of work for a beginner :+1:  but we pushing :fire:

--- Javascript-projects/html-proj/first/rock-paper-scissors/rockPaperScissor.js
+++ Javascript-projects/html-proj/first/rock-paper-scissors/rockPaperScissor.js
@@ -9,8 +9,8 @@
             'images/paper.png'
     ]
 
-    let computer_input_score = 0;
-    let user_input_score = 0;
+    let computer_score = 0;
+    let user_score = 0;
 
     //  Get all the li elements inside of the choices class
     let choicesOfPlayer = document.querySelector(".choices").getElementsByTagName('li');
@@ -21,11 +21,11 @@
     }
     
     // setCpuChoice logic here:
+    // self-explanatory with the random 
         const setCpuChoice = async() =>{
         const cpuChoice = Math.floor(Math.random()*playArray.length)
         return cpuChoice;
         }
-    console.log(setCpuChoice());
     // set the user choice function with a promise intact
     // first we loop through the indices of choicesofplayer array according to its length
     //  then we assign a simple event listener to each of them 
@@ -37,22 +37,34 @@
     }})
     const evaluateLogic = async(userChoice, cpuChoice) =>{
 
+        if (userChoice === cpuChoice) {
+            updateRound()
+          } else if (
+            (userChoice === 0 && cpuChoice === 1) ||
+            (userChoice === 2 && cpuChoice === 0) ||
+            (userChoice === 1 && cpuChoice === 2)
+          ) {
+            updateRound()
+            return user_score += 1;
+            
+          } else {
+            updateRound()
+            return computer_score+=1;
+          }
+
     }
     // creating a new function... evaluateChoices to the returned promise object from setUserChoice
     // the choice is logged for now for testing purposes
-    const evaluateChoices =() => 
-    {setUserChoice.then((userChoice) => {
-        
-        // setcpuchoice here    
+    const playGame = async() => 
+    {
+       let userChoice = await setUserChoice;
+       let cpuChoice = await setCpuChoice();
 
+        // evaluation of the choices
+        evaluateLogic(userChoice, cpuChoice);
+        console.log(`${userChoice} , ${cpuChoice}`);
 
-
-        // final evaluate logic
-        
-    }
-    
-    
-        )};
+    };
 
 
     //  roundCount Logic:
@@ -61,7 +73,7 @@
     //  after getting access with the .round-count class i assign the variable currentRound into it.
     
     let roundCount = document.querySelector(".round-count");
-    let currentRound = 0;
+    let currentRound = 1;
     roundCount.textContent = currentRound;
 
     //  updateRound logic:
@@ -77,7 +89,7 @@
 
    
 
-    const getRandomImage = () =>{
+const getRandomImage = () =>{
         const randomIndex = Math.floor(Math.random() * meme_pics.length);
         return meme_pics[randomIndex];
       } 
@@ -92,6 +104,6 @@
         randomImageUser.src = imageRandomIndexUser;
 }
   // playLogic
-    updateRound();
-    evaluateChoices();
     setRandomImage();
+    playGame();
+    
