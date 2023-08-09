da9cbc61bad98473db157900296a95654128b3fd
Josh <officialdarkey02@yahoo.com>
2023-08-09 20:03:22 +0800

    Trying out classes and objects and async functionalities

--- Javascript-projects/html-proj/first/rock-paper-scissors/play-game.js
+++ Javascript-projects/html-proj/first/rock-paper-scissors/play-game.js
@@ -1,13 +1,31 @@
-class PlayGame{
-    constructor(userchoice,cpuchoice){
-        
-        this.userchoice = userchoice;
-        this.cpuchoice = cpuchoice;
-        this.winCondition = 5;
-
+class PlayGame {
+    constructor() {
+        this.elements = document.querySelector(".choices").getElementsByTagName("li");
+        this.wincondition = 5;
+    }
+    
+    setUserChoice = () => {
+        return new Promise((resolve) => {
+            for (let index = 0; index < this.elements.length; index++) {
+                this.elements[index].addEventListener("click", () => {
+                    resolve(index);
+                });
+            }
+        });
     }
 
-   
-      
+    setCpuChoice = async () => {
+        return new Promise((resolve) => {
+            for (let index = 0; index < this.elements.length; index++) {
+                let randomIndex = Math.floor(Math.random() * this.elements.length);
+                resolve(randomIndex);
+            }
+        });
+    }
     
+    choiceEvaluate = async () => {
+        let userchoice = await this.setUserChoice();
+        let cpuchoice = await this.setCpuChoice();
+        console.log(`${userchoice} ${cpuchoice}`);
+    }
 }
--- Javascript-projects/html-proj/first/rock-paper-scissors/rockPaperScissor.js
+++ Javascript-projects/html-proj/first/rock-paper-scissors/rockPaperScissor.js
@@ -1,41 +1,37 @@
-    const meme_pics  = [
-        'images/handsome-squidward.jpg',
-        'images/mike.jpg',
-        'images/pepe.jpg'
-    ]
-    const playArray = [
-            'images/rock.png',
-            'images/scissor.png',
-            'images/paper.png'
-    ]
-// initialize all the dom elements
-let roundcount = document.querySelector(".round-count");
-let choices = document.querySelector(".choices").getElementsByTagName("li");
+      const meme_pics  = [
+          'images/handsome-squidward.jpg',
+          'images/mike.jpg',
+          'images/pepe.jpg'
+      ]
+      const playArray = [
+              'images/rock.png',
+              'images/scissor.png',
+              'images/paper.png'
+      ]
+  // initialize all the dom elements
+  let roundcount = document.querySelector(".round-count");
+  let choices = document.querySelector(".choices").getElementsByTagName("li");
+  console.log(choices);
 
-// initialize objects
-const randomImgCpu = new ArrayFunc(meme_pics);
-const randomImgUser = new ArrayFunc(meme_pics);
-const choicesOfPlayer = new ArrayFunc(playArray);
-let roundCount = new RoundCounter(roundcount);
-// Setting images for every img element
-randomImgCpu.setSrc(document.querySelector(".cpu-img"));
-randomImgUser.setSrc(document.querySelector(".user-img"));
-choicesOfPlayer.setImgs(choices);
 
-// round functionalities
-roundCount.roundUpdate();
+  // initialize objects
+  const randomImgCpu = new ArrayFunc(meme_pics);
+  const randomImgUser = new ArrayFunc(meme_pics);
+  const choicesOfPlayer = new ArrayFunc(playArray);
+  let roundCount = new RoundCounter(roundcount);
 
-const setUserChoice = async() =>{
-  return new Promise((resolve) =>{
-    for(let i = 0; i<choices.length; i++){
-      choices[i].addEventListener("click", () =>{
-        resolve(i);
-      })
-    
-    }
-    
-  })
-}
 
-let userChoice = await setUserChoice();
-console.log(userChoice);
+  // Setting images for every img element
+  randomImgCpu.setSrc(document.querySelector(".cpu-img"));
+  randomImgUser.setSrc(document.querySelector(".user-img"));
+  choicesOfPlayer.setImgs(choices);
+
+  // round functionalities
+  roundCount.roundUpdate();
+  //  game being started
+  let game = new PlayGame();
+
+  game.choiceEvaluate();
+
+
+
