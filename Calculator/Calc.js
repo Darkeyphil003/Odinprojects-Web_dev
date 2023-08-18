bc25f644bcf99eac3c95dc5510ab797c7e99a83b
Josh <officialdarkey02@yahoo.com>
2023-08-18 19:04:21 +0800

    refactored the code a little bit

--- Calculator/Calc.js
+++ Calculator/Calc.js
@@ -1,142 +1,69 @@
-// intializing Dom Elements
-const buttonContainer = document.querySelectorAll(".button-container [type = button]")
-const display = document.querySelector(".display-block")
-const clearButton = document.querySelector("#button-clear")
-
-const operators = {
-  "+": () =>{
-    const valueOperator = display.value.indexOf("+")
-    if(valueOperator!== -1){
-      const beforeOperator = display.value.split(0,valueOperator);
-      const afterOperator = display.value.split(valueOperator + 1);
-      if (display.value.includes(`+`)) {
-        console.error("+ already exists");
-      }
-      else if(beforeOperator === '' || afterOperator === ''){
-        console.error("invalid expression");
-      }
-      else {
-        display.value+=`+`
-      }
-    }
-    else {
-      display.value+=`+`
-    }
-  } ,"-": () =>{
-    const valueOperator = display.value.indexOf("-")
-    if(valueOperator!== -1){
-      const beforeOperator = display.value.split(0,valueOperator);
-      const afterOperator = display.value.split(valueOperator + 1);
-      if (display.value.includes(`+`)) {
-        console.error("- already exists");
-      }
-      else if(beforeOperator === '' || afterOperator === ''){
-        console.error("invalid expression");
-      }
-      else {
-        display.value+=`-`
-      }
-    }
-    else {
-      display.value+=`-`
-    }
-  } ,"x": () =>{
-    const valueOperator = display.value.indexOf("x")
-    if(valueOperator!== -1){
-      const beforeOperator = display.value.split(0,valueOperator);
-      const afterOperator = display.value.split(valueOperator + 1);
-      if (display.value.includes(`x`)) {
-        console.error("x already exists");
-      }
-      else if(beforeOperator === '' || afterOperator === ''){
-        console.error("invalid expression");
-      }
-      else {
-        display.value+=`x`
-      }
-    }
-    else {
-      display.value+=`x`
-    }
-  } ,"/": () =>{
-    const valueOperator = display.value.indexOf("/")
-    if(valueOperator!== -1){
-      const beforeOperator = display.value.split(0,valueOperator);
-      const afterOperator = display.value.split(valueOperator + 1);
-      if (display.value.includes(`/`)) {
-        console.error("/ already exists");
-      }
-      else if(beforeOperator === '' || afterOperator === ''){
-        console.error("invalid expression");
-      }
-      else {
-        display.value+=`/`
-      }
-    }
-    else {
-      display.value+= '/'
-    }
+class Calculator{
+  constructor(display) {
+   this.display = display;
   }
-  , ".": () =>{
-    //  checks for the button . 
-    // @value would be a floating point 
-        const currentDisplay = display.value
-        const dotOperator = display.value.indexOf(".")
-        const beforeOperator = display.value.split(0,dotOperator);
-        const afterOperator = display.value.split(dotOperator + 1);
-        
-      
-        if(currentDisplay === "" || operators.hasOwnProperty(currentDisplay[currentDisplay.length-1])){
-          if(currentDisplay.includes("0.")){
-            console.error("There is already a 0.");
-          }
-          else {
-          display.value += "0."
-          }
-
-        }
-   
-          
-        
-       
-    
+  clear(){
+     this.display.value = ""
   }
-}
+  delete(){
+    this.display.value = this.display.value.slice(0,-1);
+  }
+  
+  selectOperator(operator){
+    const currentIndex = this.display.value.indexOf(operator);
+    this.previousExpression = this.display.value.slice(0,currentIndex);
+    this.nextExpression = this.display.value.slice(currentIndex);
 
-// getting All the button elements
-function Calculator() {
-    // clearing the screen
-    clearButton.addEventListener("click" ,() =>{
-        display.value = "";
-    })
-    display.addEventListener("mouseenter", () =>{
-      display.setAttribute("placeholder" , "")
-    })
-    display.addEventListener("mouseleave", () =>{
-      display.setAttribute("placeholder" , "0")
-    })
-    // event listener for every button 
-   for (let index = 0; index < buttonContainer.length; index++) {
-    // @param buttoncontainer[index] is the specific button inside the buttonContainer
-    buttonContainer[index].addEventListener("click" , () =>{
-      
-        
-   
-      if (buttonContainer[index].value === "=") {
-        
-      } 
-     
-      else if(operators.hasOwnProperty(buttonContainer[index].value)){
-          operators[buttonContainer[index].value]()
-      }
+  }
+ }
 
-      else {
-        display.value+=buttonContainer[index].value
-      }
-      
-       
-    })
+ const operators = {
+  "+":() => {
+    if(Calc.display.value.includes(Object.keys(operators)[0])) return;
+    else Calc.display.value += Object.keys(operators)[0]
     
+  }, 
+  "/": ()=>{
+    if(Calc.display.value.includes(Object.keys(operators)[1])) return;
+    else Calc.display.value += Object.keys(operators)[1]
+  }, 
+  "x":()=>{
+    if(Calc.display.value.includes(Object.keys(operators)[2])) return;
+    else Calc.display.value += Object.keys(operators)[2]
+  },
+   "-":()=>{
+    if(Calc.display.value.includes(Object.keys(operators)[3])) return;
+    else Calc.display.value += Object.keys(operators)[3]
+  },
+   ".":()=>{
+    if(Calc.display.value.includes(Object.keys(operators)[4])) return;
+    else Calc.display.value += Object.keys(operators)[4]
+  }
+ }
+
+const buttons = document.querySelectorAll(".button-container [type = button]");
+const display = document.querySelector(".display-block");
+const clearButton = document.querySelector(".forms [value = C]");
+const deleteButton = document.querySelector(".forms [value = delete]");
+// creating a calculator object
+
+const Calc = new Calculator(display);
+
+// Adding event listener to every button
+buttons.forEach(button => {
+  button.addEventListener("click", () =>{
+   
+   if (operators.hasOwnProperty(button.value)) {
+    operators[button.value]();
    }
-}
-Calculator();   
+   else Calc.display.value += button.value
+    
+  })
+});
+
+clearButton.addEventListener("click", ()=>{
+  Calc.clear()
+})
+deleteButton.addEventListener("click", ()=>{
+  Calc.delete()
+})
