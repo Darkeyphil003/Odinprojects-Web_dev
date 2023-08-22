e63dcdf2409ed400bf6b6dfcf86a62958d9c77ab
Josh <officialdarkey02@yahoo.com>
2023-08-22 19:40:55 +0800

    CSS and HTML Done!

--- Tic-Tac-Toe/Tic-Tac-Toe.js
+++ Tic-Tac-Toe/Tic-Tac-Toe.js
@@ -1,3 +1,18 @@
-const bruh = `${bruh}`
 
-console.log(bruh);
+document.addEventListener("DOMContentLoaded", () =>{
+    let playerScore = document.getElementById("player-score");
+    let cpuScore = document.getElementById("cpu-score");
+    let tiesCount = document.getElementById("tieValue");
+
+    let scores ={
+        playerScoreValue:0, 
+        cpuScoreValue:0,
+        ties:0
+    }
+    cpuScore.textContent = scores.cpuScoreValue;
+    playerScore.textContent = scores.playerScoreValue;
+    tiesCount.textContent = scores.ties
+})
+
+
+
